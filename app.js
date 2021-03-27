7743d3785bc7072cc10d855e268c8ec6a16cd12b
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-03-26 22:32:53 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+
+//load config
+dotenv.config({ path: './config/config.env' })
+
+connectDB();
+
+const app = express()
+
+//logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+//handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//routes
+app.use('/', require('./routes/index'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    )
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+// @desc login/landing page
+// @route GET /
+router.get('/', (req, res) => {
+    res.render('login', {
+        layout: 'login'
+    })
+})
+
+// @desc dashboard
+// @route GET /dashboard
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+})
+
+module.exports = router;
