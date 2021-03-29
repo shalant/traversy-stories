fa9ca7fbd3d3edbbc505acd06efab466738d0f00
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-03-29 09:23:07 -0500

    finished this nice traversy video

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
         })
 
         console.log(`MongoDB Connected: ${conn.connection.host}`)
-    } catch (error) {
+    } catch (err) {
         console.error(err)
         process.exit(1)
     }
